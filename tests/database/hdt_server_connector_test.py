4f3f8362358fefb630360e1795696da7deb37989
Thomas Minier <tminier01@gmail.com>
2018-05-27 09:33:18 +0200

    disable HDTServer tests due to instability of the live HDT server

--- tests/database/hdt_server_connector_test.py
+++ tests/database/hdt_server_connector_test.py
@@ -1,10 +1,12 @@
 # rdf_file_connector_test.py
 # Author: Thomas MINIER - MIT License 2017-2018
 from database.hdt_server_connector import HDTServerConnector
+import pytest
 
 db = HDTServerConnector("https://hdt.lod.labs.vu.nl", "<https://hdt.lod.labs.vu.nl/graph/Wikidata>")
 
 
+@pytest.mark.skip(reason="broken due to an issue with the live HDT server")
 def test_simple_search_hdtserver():
     it, card = db.search_triples("http://www.wikidata.org/entity/P2842", None, None)
     nbRead = 0
@@ -15,6 +17,7 @@ def test_simple_search_hdtserver():
     assert nbRead == card
 
 
+@pytest.mark.skip(reason="broken due to an issue with the live HDT server")
 def test_limit_search_hdtserver():
     it, card = db.search_triples("http://www.wikidata.org/entity/P2842", None, None, limit=110)
     nbRead = 0
@@ -25,6 +28,7 @@ def test_limit_search_hdtserver():
     assert nbRead == 110
 
 
+@pytest.mark.skip(reason="broken due to an issue with the live HDT server")
 def test_offset_search_hdtserver():
     it, card = db.search_triples("http://www.wikidata.org/entity/P2842", None, None, offset=105)
     nbRead = 0
