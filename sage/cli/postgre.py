fbadcfbf3c8da6ab64a56dc3c0284c77688eb62e
Thomas Minier <tminier01@gmail.com>
2019-03-08 16:40:05 +0100

    add optional indexing & a command to build indexes separately

--- sage/cli/postgre.py
+++ sage/cli/postgre.py
@@ -27,7 +27,8 @@ def connect_postgre(dataset):
 @click.command()
 @click.argument("config")
 @click.argument("dataset_name")
-def init_postgre(config, dataset_name):
+@click.option('--index/--no-index', default=False, help="Enable/disable indexing of SQL tables. The indexes can be created separately using the command sage-postgre-index")
+def init_postgre(config, dataset_name, build_index):
     """
         Initialize the RDF dataset DATASET_NAME with a PostgreSQL backend, described in the configuration file CONFIG.
     """
@@ -61,16 +62,60 @@ def init_postgre(config, dataset_name):
     # TODO
 
     # create the additional inexes on OSP and POS
-    logger.info("Creating additional B-tree indexes...")
-    for q in create_indexes_queries:
-        cursor.execute(q)
-    logger.info("Additional B-tree indexes successfully created")
+    if build_index:
+        logger.info("Creating additional B-tree indexes...")
+        for q in create_indexes_queries:
+            cursor.execute(q)
+        logger.info("Additional B-tree indexes successfully created")
+    else:
+        logger.info("Skipping additional indexes creation on user-demand")
 
     # create the cursor functions used to perform index scans
     logger.info("Creating utility PL-SQL functions...")
     for q in create_cursors_queries:
         cursor.execute(q)
     logger.info("Utility PL-SQL functions successfully created")
+
+    # commit and cleanup connection
+    logger.info("Committing and cleaning up...")
+    connection.commit()
+    cursor.close()
+    connection.close()
+    logger.info("Sage PostgreSQL model for table {} successfully initialized".format(table_name))
+
+
+@click.command()
+@click.argument("config")
+@click.argument("dataset_name")
+def index_postgre(config, dataset_name):
+    """
+        Create the additional B-tree indexes on the RDF dataset DATASET_NAME, described in the configuration file CONFIG. The dataset must use the PostgreSQL backend.
+    """
+    # install logger
+    coloredlogs.install(level='INFO', fmt='%(asctime)s - %(levelname)s %(message)s')
+    logger = logging.getLogger(__name__)
+
+    # load dataset from config file
+    dataset = load_dataset(config, dataset_name, 'postgre', logger)
+
+    # init postgre connection
+    connection = connect_postgre(dataset)
+    if connection is None:
+        exit(1)
+    # turn off autocommit
+    connection.autocommit = False
+    # create all SQL queries used to init the dataset, using the dataset name
+    table_name = dataset['name']
+    create_indexes_queries = p_utils.get_postgre_create_indexes(table_name)
+
+    # create indexes
+    cursor = connection.cursor()
+    start = time()
+    logger.info("Creating additional B-tree indexes...")
+    for q in create_indexes_queries:
+        cursor.execute(q)
+    stop = time()
+    logger.info("Additional B-tree indexes successfully created in {}s".format(stop - start))
 
     # commit and cleanup connection
     logger.info("Committing and cleaning up...")
--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ POSTGRESQL_DEPS = [
 console_scripts = [
     'sage = sage.cli.start_server:start_sage_server',
     'sage-postgre-init = sage.cli.postgre:init_postgre',
+    'sage-postgre-index = sage.cli.postgre:index_postgre',
     'sage-postgre-put = sage.cli.postgre:put_postgre'
 ]
 
